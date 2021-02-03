da8cc20e0bb6c12b6530151310a3a6e3cff49d45
FedorLivshits <fediliv@gmail.com>
2021-02-03 18:18:52 +0300

    Added Header component, backgrounds and Header style

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import './Header.css'
+
+function Header () {
+    return (
+        <header className="header z-depth-2 indigo darken-3">
+            <div className="container">
+                <div className="header__inner">
+                    <div className="header-logo">
+                        <a href="#">
+                            <img src="images" alt=""/>
+                        </a>
+                    </div>
+                    <div className="header-search__line input-field">
+                        <input type="search" placeholder="Поиск"/>
+                    </div>
+                    <div className="header-log-in">
+                        <div className="header-btn">
+                            <a className="login" href="#">LOGIN</a>
+                            <a className="sign-up yellow darken-2" href="#">SIGN UP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+export default Header;
