ab10c35964335693db1c47c66b39004788a6e872
FedorLivshits <fediliv@gmail.com>
2021-02-21 13:59:56 +0300

    now you can edit your status

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App(props) {
                     <Route path='/login' render={() => <Login />}/>
                 </div>
             </div>
+
         </div>
     );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,39 +7,80 @@ import Preloader from "../Preloader/Preloader";
 
 
 function Profile(props) {
-    if(!props.profile){
+    if (!props.profile) {
         return <Preloader/>
     }
     return (
         <section className="profile__content">
             <div className="color__wrapper z-depth-2">
-            <div className="profile__inner z-depth-2">
-                <div className="profile__photo-box">
-                    <div className="profile__photo">
-                        <img src={(props.profile.photos.large != null) ? props.profile.photos.large : (props.match.params.userId === 15052) ? photo1 : photo} alt=""/>
+                <div className="profile__inner z-depth-2">
+                    <div className="profile__photo-box">
+                        <div className="profile__photo">
+                            <img
+                                src={(props.profile.photos.large != null) ? props.profile.photos.large : (props.match.params.userId === 15052) ? photo1 : photo}
+                                alt=""/>
+                        </div>
+                        <div className="profile__photo-btn">
+                            <a className="btn-floating btn-large waves-effect waves-light btn yellow darken-2"><i
+                                className="material-icons">add</i></a>
+                        </div>
                     </div>
-                    <div className="profile__photo-btn">
-                        <a className="btn-floating btn-large waves-effect waves-light btn yellow darken-2"><i
-                            className="material-icons">add</i></a>
+
+                    <div className="profile-text__info">
+                        <div className="title__info">
+                            {props.profile.fullName}
+                        </div>
+
+                        <ProfileStatus status={'hello my friends'}/>
+
+                        <div className="profile___info-descr">
+                            <li><b>ABOUT ME:</b> {props.profile.aboutMe}</li>
+                            <li><b>LOOKING FOR A JOB:</b> {props.profile.lookingForAJobDescription}</li>
+                        </div>
                     </div>
                 </div>
 
-                <div className="profile-text__info">
-                    <div className="title__info">
-                        {props.profile.fullName}
-                    </div>
-                    <div className="profile___info-descr">
-                        <li> <b>ABOUT ME:</b> {props.profile.aboutMe}</li>
-                        <li> <b>LOOKING FOR A JOB:</b> {props.profile.lookingForAJobDescription}</li>
-                    </div>
-                </div>
-            </div>
-
-            <MyPostsContainer store={props.store}/>
+                <MyPostsContainer store={props.store}/>
 
             </div>
         </section>
     );
+}
+
+class ProfileStatus extends React.Component {
+
+    state = {
+        editMode: false,
+    }
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return (
+            <div className="profile-status">
+                {!this.state.editMode ?
+                    <div className="status-text" onClick={this.activateEditMode}>
+                        {this.props.status}
+                    </div> :
+                    <div className="status-form">
+                        <div className="input-field col s6">
+                            <input autoFocus={true} id="input_text" type="text" placeholder="status" value={this.props.status}
+                                   onBlur={this.deactivateEditMode}/>
+                        </div>
+                    </div>}
+
+            </div>
+        )
+    }
+
 }
 
 export default Profile;
