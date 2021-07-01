e2efdc6be61f64570edc3b50c55b84b976124079
FedorLivshits <fediliv@gmail.com>
2021-07-02 01:32:04 +0300

    typescript for api

---
+++ src/api/profile-api.ts
@@ -0,0 +1,40 @@
+import {PhotosType, ProfileType} from "../types/types";
+import {instance, APIResponseType} from "./api";
+
+
+type SavePhotoResponseType = {
+    photos: PhotosType
+}
+
+export const profileAPI = {
+    getProfile(userId: number) {
+        return (
+            instance.get<ProfileType>(`profile/` + userId)
+                .then(res => res.data)
+        )
+    },
+    getStatus(userId: number) {
+        return (
+            instance.get<string>(`profile/status/` + userId)
+                .then(res => res.data)
+        )
+    },
+    updateStatus(status: string) {
+        return (
+            instance.put<APIResponseType>(`profile/status/`, {status: status})
+                .then(res => res.data)
+        )
+    },
+    savePhoto(photo: File) {
+        const formData = new FormData()
+        formData.append("image", photo)
+        return (
+            instance.put<APIResponseType<SavePhotoResponseType>>('profile/photo', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }))
+            .then(res => res.data)
+    }
+}
+
