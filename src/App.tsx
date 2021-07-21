938d30537b893784b0002420e9e00ca99b51366f
Fedor Livshits <fediliv@gmail.com>
2021-07-21 22:53:43 +0300

    refactor App component to useSelector/useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { lazy, Suspense, useEffect } from 'react';
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Route, withRouter } from "react-router-dom";
 import { compose } from "redux";
 import DialogsContainer from "./components/Pages/DialogsPage/DialogsContainer";
-import Login from "./components/Pages/LoginPage/Login";
+import { Login } from "./components/Pages/LoginPage/Login";
 import PostsPage from "./components/Pages/PostsPage/PostsPage";
-import SavedPage from "./components/Pages/SavedPage/SavedPage";
+import { SavedPage } from "./components/Pages/SavedPage/SavedPage";
 import Preloader from "./components/Preloader/Preloader";
-import SidebarContainer from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import './main.css';
 import { initializeApp } from "./redux/app-reducer";
 import { AppStateType } from "./redux/redux-store";
@@ -16,22 +16,20 @@ import { AppStateType } from "./redux/redux-store";
 const ProfilePage = lazy(() => import("./components/Pages/ProfilePage/ProfilePage"))
 const UsersPage = lazy(() => import("./components/Pages/UsersPage/UsersPage"))
 
-type MapStatePropsType = ReturnType<typeof mapStateToProps>
-type MapDispatchToPropsType = {
-    initializeApp: () => void
-}
 
-const App: React.FC<MapStatePropsType & MapDispatchToPropsType> = (props) => {
+const App: React.FC = () => {
+    const initialized = useSelector((state: AppStateType) => state.app.initialized)
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        props.initializeApp()
+        dispatch(initializeApp())
     }, [])
 
-    if (!props.initialized) return <Preloader />
+    if (!initialized) return <Preloader />
 
     return (
         <>
-            <SidebarContainer /> 
+            <Sidebar />
             <div className="home__content">
                 <Route exact path='/profile/:userId?' render={() => {
                     return <Suspense fallback={<Preloader />}>
@@ -47,15 +45,12 @@ const App: React.FC<MapStatePropsType & MapDispatchToPropsType> = (props) => {
                 <Route path='/usersPosts' render={() => <PostsPage />} />
                 <Route path='/saved' render={() => <SavedPage />} />
                 <Route path='/login' render={() => <Login />} />
-                <Redirect from='/' to='/profile'/>
+                <Redirect from='/' to='/profile' />
             </div>
         </>
     );
 }
 
-const mapStateToProps = (state: AppStateType) => ({
-    initialized: state.app.initialized
-})
 
-export default compose(withRouter, connect(mapStateToProps, { initializeApp }))(App) as React.ComponentType;
+export default compose(withRouter)(App) as React.ComponentType;
 
