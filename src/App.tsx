4d556800252fccb3f4725698a3d5cba907c9a84a
Fedor Livshits <fediliv@gmail.com>
2021-10-01 03:57:52 +0300

    add NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { AnimatePresence } from 'framer-motion'
 import React, { lazy, Suspense, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Route, withRouter } from 'react-router-dom'
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom'
 import { compose } from 'redux'
 import FooterMobile from './components/FooterMobile/FooterMobile'
 import DialogsContainer from './components/Pages/DialogsPage/DialogsContainer'
@@ -14,11 +14,13 @@ import './main.scss'
 import { initializeApp } from './redux/app-reducer'
 import { AppStateType } from './redux/redux-store'
 import FriendsPage from './components/Pages/FriendsPage/FriendsPage'
+import NotFound from './components/Pages/NotFound/NotFound'
 
 const ProfilePage = lazy(
 	() => import('./components/Pages/ProfilePage/ProfilePage')
 )
 const UsersPage = lazy(() => import('./components/Pages/UsersPage/UsersPage'))
+
 
 const App: React.FC = () => {
 	const initialized = useSelector(
@@ -37,33 +39,36 @@ const App: React.FC = () => {
 			<Sidebar />
 			<div className='home__content'>
 				<AnimatePresence>
-					<Route
-						exact
-						path='/profile/:userId?'
-						render={() => {
-							return (
-								<Suspense fallback={<Preloader />}>
-									<ProfilePage />
-								</Suspense>
-							)
-						}}
-					/>
-					<Route
-						path='/users'
-						render={() => {
-							return (
-								<Suspense fallback={<Preloader />}>
-									<UsersPage />
-								</Suspense>
-							)
-						}}
-					/>
-					<Route path='/dialogs' render={() => <DialogsContainer />} />
-					<Route path='/usersPosts' render={() => <PostsPage />} />
-					<Route path='/saved' render={() => <SavedPage />} />
-					<Route path='/login' render={() => <Login />} />
-					<Route path='/friends' render={() => <FriendsPage />} />
-					{/*<Redirect from='/' to='/profile' />*/}
+					<Switch>
+						<Route
+							exact
+							path='/profile/:userId?'
+							render={() => {
+								return (
+									<Suspense fallback={<Preloader />}>
+										<ProfilePage />
+									</Suspense>
+								)
+							}}
+						/>
+						<Route
+							path='/users'
+							render={() => {
+								return (
+									<Suspense fallback={<Preloader />}>
+										<UsersPage />
+									</Suspense>
+								)
+							}}
+						/>
+						<Route path='/dialogs' render={() => <DialogsContainer />} />
+						<Route path='/usersPosts' render={() => <PostsPage />} />
+						<Route path='/saved' render={() => <SavedPage />} />
+						<Route path='/login' render={() => <Login />} />
+						<Route path='/friends' render={() => <FriendsPage />} />
+                        <Route component={NotFound} />
+						<Redirect from='/' to='/profile' />
+					</Switch>
 				</AnimatePresence>
 			</div>
 			<FooterMobile />
